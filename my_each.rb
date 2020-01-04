4e8a480e7650e401fd23bcb1cc24dcb807d2a87a
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-04 20:15:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,27 @@
-def my_each(array)
-  i = 0
+# def my_each(array)
+#   i = 0
   
-  while i < array.length 
-    yield array[i] 
-    i = i + 1
-  end
-  array
-end
+#   while i < array.length 
+#     yield array[i] 
+#     i = i + 1
+#   end
+#   array
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
